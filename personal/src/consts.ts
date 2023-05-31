755a3c05e2912fa0e199a3766ba90627deae9b94
Darián López Utra <daroxs95@gmail.com>
2023-05-30 22:15:05 -0400

    Add linkedin

--- personal/src/consts.ts
+++ personal/src/consts.ts
@@ -5,3 +5,6 @@ export const MY_NAME = "Darián López Utra";
 export const SITE_TITLE = "Daro's wisdom";
 export const SITE_DESCRIPTION = "Anything and everything attempt of blog";
 export const PRODUCTS_PAGE_DESCRIPTION = "Products, apps and tools list to cybernetically enhance your life";
+export const PERSONAL_GITHUB = "https://github.com/daroxs95";
+export const PERSONAL_LINKEDIN = "https://www.linkedin.com/in/darian-lopez-utra/";
+
